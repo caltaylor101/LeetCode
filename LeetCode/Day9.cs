0423f99dd4e45c69208babefb70febf24359776d
caltaylor <Caltaylor101@gmail.com>
2022-07-14 22:11:07 -0600

    More DFS and recursive patterns on matrices.

---
+++ LeetCode/Day9.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class Day9
+    {
+        private bool srDone;
+        private bool scDone;
+        /*public int[][] FloodFill2(int[][] image, int sr, int sc, int color)
+        {
+            return image;
+        }
+
+        public int[][] FloodFill(int[][] image, int sr, int sc, int color)
+        {
+            if (image.Length == 0) return image;
+            if (image[sr][sc] == color) return image;
+
+
+            bool down = sr + 1 < image.Length && image[sr][sc] == image[sr + 1][sc];
+            bool up = sr - 1 >= 0 && image[sr - 1][sc] == image[sr][sc];
+            bool right = sc + 1 < image[sr].Length && image[sr][sc + 1] == image[sr][sc];
+            bool left = (sc - 1 >= 0 && image[sr][sc - 1] == image[sr][sc]);
+
+
+            image[sr][sc] = color;
+
+
+            if (down) FloodFill(image, sr + 1, sc, color);
+            if (up)  FloodFill(image, sr - 1, sc, color);
+            if (right) FloodFill(image, sr, sc + 1, color);
+            if (left) FloodFill(image, sr, sc - 1, color);
+
+            return image;
+        }*/
+        public int[][] FloodFill(int[][] image, int sr, int sc, int color)
+        {
+            int startColor = image[sr][sc];
+            return RecurseFill(image, sr, sc, color, startColor);
+        }
+
+        public int[][] RecurseFill(int[][] image, int sr, int sc, int color, int startColor)
+        {
+            if (startColor == color)
+            {
+                return image;
+            }
+            if (image[sr][sc] == startColor)
+            {
+                image[sr][sc] = color;
+            }
+            if (sr - 1 >= 0 && image[sr - 1][sc] == startColor)
+            {
+                image[sr-1][sc] = color;
+                RecurseFill(image, sr - 1, sc, color, startColor);
+            }
+            if (sr + 1 < image.Length && image[sr + 1][sc] == startColor)
+            {
+                image[sr + 1][sc] = color;
+                RecurseFill(image, sr + 1, sc, color, startColor);
+            }
+            if (sc - 1 >=0 && image[sr][sc - 1] == startColor)
+            {
+                image[sr][sc - 1] = color;
+                RecurseFill(image, sr, sc - 1, color, startColor);
+            }
+            if (sc + 1 < image[sr].Length && image[sr][sc+1] == startColor)
+            {
+                image[sr][sc + 1] = color;
+                RecurseFill(image, sr, sc + 1, color, startColor);
+            }
+            
+
+            return image;
+        }
+
+        /*public int[][] FloodFill(int[][] image, int sr, int sc, int color)
+        {
+            if (image.Length == 0) return image;
+            if (image[sr][sc] == color) return image;
+
+            bool down = (sr + 1) < image.Length && sr >= 0 && image[sr][sc] == image[sr + 1][sc];
+            bool up = (sr - 1) < image.Length && sr >= 1 && image[sr][sc] == image[sr - 1][sc];
+            bool right = (sc + 1) < image[sr].Length && sc >= 0 && image[sr][sc] == image[sr][sc + 1];
+            bool left = (sc - 1) < image[sr].Length && sc >= 1 && image[sr][sc] == image[sr][sc - 1];
+
+            image[sr][sc] = color;
+
+            if (down) image = FloodFill(image, sr + 1, sc, color);
+            if (up) image = FloodFill(image, sr - 1, sc, color);
+            if (right) image = FloodFill(image, sr, sc + 1, color);
+            if (left) image = FloodFill(image, sr, sc - 1, color);
+
+            return image;
+
+        }*/
+        private int islands = 0;
+        public int NumIslands(char[][] grid)
+        {
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    if (grid[i][j] == '1')
+                    {
+                        islands += RecurseIsland(grid, i, j);
+                    }
+                }
+            }
+            return islands;
+        }
+
+        public int RecurseIsland(char[][] grid, int x, int y)
+        {
+            if (x-1 >= 0 && grid[x - 1][y] == '1')
+            {
+                grid[x - 1][y] = '0';
+                RecurseIsland(grid, x - 1, y);
+            }
+            if (x + 1 < grid.Length && grid[x + 1][y] == '1')
+            {
+                grid[x + 1][y] = '0';
+                RecurseIsland(grid, x + 1, y);
+            }
+            if (y - 1 >= 0 && grid[x][y - 1] == '1')
+            {
+                grid[x][y - 1] = '0';
+                RecurseIsland(grid, x, y - 1);
+            }
+            if (y + 1 < grid[x].Length && grid[x][y + 1] == '1')
+            {
+                grid[x][y + 1] = '0';
+                RecurseIsland(grid, x, y + 1);
+            }
+
+            return 1;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -2,17 +2,42 @@
 using LeetCode;
 
 
-Day6 day = new Day6();
-
+Day9 day = new Day9();
+/*
 TreeNode node1 = new TreeNode(25);
 TreeNode node2 = new TreeNode(56);
 TreeNode node3 = new TreeNode(19, null, node1);
 
 TreeNode node4 = new TreeNode(47, null, node2);
 TreeNode node5 = new TreeNode(26, node3);
-TreeNode node6 = new TreeNode(32, node5, node4);
+TreeNode node6 = new TreeNode(32, node5, node4);*/
 
 //day.LevelOrder(node5);
 
 
-Console.WriteLine(day.IsValidBST(node6));
+/*int[][] image = new int[][] {
+    new int[] { 1, 1, 1 },
+    new int[] { 1, 1, 0 },
+    new int[] { 1, 0, 1 }
+};*/
+/*int[][] image = new int[][] {
+    new int[] { 0,0,0 },
+    new int[] { 0,1,0 }
+};
+*/
+/*int[][] image = new int[][]
+{
+    new int[] {0,0,0},
+    new int[] {0,0,0}
+};*/
+
+char[][] islandLands = new char[][]
+{
+    new char[] {'1','1','1', '0','0'},
+    new char[] {'0','0','0', '1','1'},
+    new char[] {'0','0','0', '0','0'},
+    new char[] {'0','1','0', '0','0'}
+};
+
+Console.WriteLine(day.NumIslands(islandLands));
+
